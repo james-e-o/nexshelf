8590feecb7c9a2c70e5f95f89ba95c9bb8fb958e
theastutedev <onwuasoanyajames@gmail.com>
2025-11-13 19:47:09 +0100

    nexshelf 13/11 main/master 2

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,32 +1,6 @@
+import { redirect } from "next/navigation";
 
-import { redirect } from 'next/navigation'
-import { createClient } from '@/utils/supabase/server'
-
-const Admin = async () => {
-  const supabase = createClient()
- 
-  const { data: { session } } = await supabase.auth.getSession()
-
-  if (!session) {
-    redirect('accounts/login')
-  }
-
-  const { data: profile, error: profileError } = await supabase
-      .from('admins')
-      .select('username, handle')
-      .eq('id', session.user.id)
-      .single()
-    // console.log('profile from admin',profile)
-    if (profileError || !profile) {
-      console.error('Profile fetch error:', profileError)
-      redirect('accounts/login')
-    }
-
-
-   redirect(`/admin/${profile.handle}`)
-
+export default function CompanyIndexRedirect({ params }) {
+  const { u } = params; // dynamic segment for user handle
+  u?redirect(`/admin/${u}`):redirect(`/accounts/login`)
 }
-
-export default Admin
-
-
