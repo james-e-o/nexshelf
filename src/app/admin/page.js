f11b219fe7cf57df648efaec38b1441edca9d327
theastutedev <onwuasoanyajames@gmail.com>
2025-11-16 19:13:03 +0100

    nexshelf 16/11 main/master 2

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-export default function CompanyIndexRedirect({ params }) {
+export default async function CompanyIndexRedirect({ params }) {
   const { u } = params; // dynamic segment for user handle
   u?redirect(`/admin/${u}`):redirect(`/accounts/login`)
 }
