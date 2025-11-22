d9d9556061cdff87d64da9ca788b2c99879eb88d
theastutedev <onwuasoanyajames@gmail.com>
2025-11-22 07:54:39 +0100

    nexshelf 22/11 main/master 2

--- src/app/admin/[u]/company/[companySlug]/staff/new/page.js
+++ src/app/admin/[u]/company/[companySlug]/staff/new/page.js
@@ -3,7 +3,7 @@ import { useState,useContext } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { CompanyInfoContext } from "@/components/contexts/company-context";
+import { CompanyInfoContext } from "../../layout";
 import { toast } from "sonner";
 
 export default function StaffOnboarding({ companyId, companyName, companyLogo }) {
